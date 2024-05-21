d59a1a4cff007e99a14621ff743b23156e006e82
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-05-21 18:51:56 +0530

    dashboard bar-chart implemented.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
+
+
