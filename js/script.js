b972c2b01a777376302c12aa2c6bf79288139d74
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-05-21 19:25:10 +0530

    date & time implemented.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const timer = document.getElementById("time-lbl");
+const date = document.getElementById("date-lbl");
+
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.querySelector(".nav-links");
@@ -7,4 +10,46 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+function getTime() {
+  let t = new Date().toLocaleTimeString();
+  timer.textContent = t;
+}
 
+function getDate() {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let now = new Date();
+  let dayName = days[now.getDay()];
+  let day = now.getDate();
+  let monthName = months[now.getMonth()];
+  let year = now.getFullYear();
+
+  let formattedDate = `${dayName}(${day})-${monthName}-${year}`;
+  date.textContent = formattedDate;
+}
+
+getDate();
+getTime();
+setInterval(getTime, 1000);
