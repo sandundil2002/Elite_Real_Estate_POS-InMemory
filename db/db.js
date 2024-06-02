6f39d37655ddefb778f62ae37e89b2c16f296b0f
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-06-02 10:50:20 +0530

    customer db implemented.

--- db/db.js
+++ db/db.js
@@ -2,15 +2,87 @@ export let Appointments = [
   {
     appId: "A001",
     adminId: "Adm001",
-    name: "sandun",
-    mobile: "762357995",
+    name: "Amal",
+    mobile: "775369891",
     dateTime: "2024-01-01T12:00",
   },
+
   {
     appId: "A002",
     adminId: "Adm002",
-    name: "sandun",
-    mobile: "762357995",
+    name: "Kamal",
+    mobile: "759842061",
     dateTime: "2024-01-01T12:00",
+  },
+
+  {
+    appId: "A003",
+    adminId: "Adm003",
+    name: "Nimal",
+    mobile: "716525698",
+    dateTime: "2024-01-01T12:00",
+  },
+
+  {
+    appId: "A004",
+    adminId: "Adm002",
+    name: "Sunil",
+    mobile: "789512065",
+    dateTime: "2024-01-01T12:00",
+  },
+
+  {
+    appId: "A005",
+    adminId: "Adm001",
+    name: "Vimal",
+    mobile: "746528046",
+    dateTime: "2024-01-01T12:00",
+  },
+];
+
+export let Customers = [
+  {
+    cusId: "C001",
+    AppId: "A001",
+    cusName: "Amal",
+    address: "Galle",
+    cusMobile: "775369891",
+    email: "amal@gmail.com",
+  },
+
+  {
+    cusId: "C002",
+    AppId: "A002",
+    cusName: "Kamal",
+    address: "Mathara",
+    cusMobile: "759842061	",
+    email: "kamal@gmail.com",
+  },
+
+  {
+    cusId: "C003",
+    AppId: "A003",
+    cusName: "Nimal",
+    address: "Panadura",
+    cusMobile: "716525698	",
+    email: "nimal@gmail.com",
+  },
+
+  {
+    cusId: "C004",
+    AppId: "A004",
+    cusName: "Sunil",
+    address: "Kandy",
+    cusMobile: "789512065	",
+    email: "sunil@gmail.com",
+  },
+
+  {
+    cusId: "C005",
+    AppId: "A005",
+    cusName: "Vimal",
+    address: "Mathale",
+    cusMobile: "746528046		",
+    email: "vimal@gmail.com",
   },
 ];
